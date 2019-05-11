a79edb6b02fcde7a67b3a98774060c1716c359f9
Mitchel Paulin <mitchel0022@gmail.com>
2019-05-11 17:55:01 -0400

    Got a general mockup of the UI finished

--- MainWindow.py
+++ MainWindow.py
@@ -1,9 +1,27 @@
-# File: main.py
+#File MainWindow.py
 import sys
 import qdarkstyle
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import QFile
+
+def populateCostComboBox(window):
+    functions = ['1', 'n', 'n^2', 'n^3']
+    speedComboBox = window.speed_cost_function_comboBox
+    sightComboBox = window.sight_cost_function_comboBox 
+    sizeComboBox = window.size_cost_function_comboBox 
+
+    for func in functions: 
+        speedComboBox.addItem(func)
+        sightComboBox.addItem(func)
+        sizeComboBox.addItem(func)
+
+    #recommended default starting values 
+    speedComboBox.setCurrentIndex(2)
+    sightComboBox.setCurrentIndex(1)
+    sizeComboBox.setCurrentIndex(3)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -11,12 +29,16 @@ if __name__ == "__main__":
     # setup stylesheet
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside())
 
+    #load ui file 
     ui_file = QFile("mainwindow.ui")
     ui_file.open(QFile.ReadOnly)
-
     loader = QUiLoader()
     window = loader.load(ui_file)
     ui_file.close()
+
+    #initialize values
+    populateCostComboBox(window)
+
     window.show()
 
     sys.exit(app.exec_())
