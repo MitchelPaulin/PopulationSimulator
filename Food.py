d23d793e03e20482d0c6b1e93828889c0223f9c3
Mitchel Paulin <mitchel0022@gmail.com>
2019-05-14 20:43:21 -0400

    Got food drawing to the screen

--- Food.py
+++ Food.py
@@ -1,18 +1,23 @@
 #File Food.py 
 #Holds a food instance 
 
-from PySide2.QtGui import QPixmap
+from PySide2.QtWidgets import QGraphicsPixmapItem
+import random
 
 class Food():
     xPos = None 
     yPos = None 
     nutritionalValue = 1
     pixmap = None 
+    foodImages = ['Cherry.png', 'Watermelon.png', 'Pear.png']
 
-    def __init__(self, xPos, yPos, image='../assets/food.png'):
+    def __init__(self, xPos, yPos, image=None):
         self.xPos = xPos
         self.yPos = yPos 
-        self.pixmap = QPixmap(image)
+        if not image:
+            self.pixmap = QGraphicsPixmapItem('assets/'+random.choice(self.foodImages))
+        else:
+            self.pixmap = QGraphicsPixmapItem(image)
 
 
 
--- SimulationView.py
+++ SimulationView.py
@@ -2,19 +2,31 @@
 #Handles the rendering of a simulation 
 
 from PySide2.QtWidgets import QGraphicsScene, QGraphicsView
+from random import randint
 import Simulation
+from Food import Food 
+from Creature import Creature
 
 class SimulationView():
 
     graphicsScene = None 
     isSimulating = False 
     beginSimulationButton = None 
+    food = None 
+    creatures = None
+    BUFFER = 10 #ensure we don't drop items too close to the extremes of the scene
 
 
     def __init__(self, mainWindow):
+
+        self.food = []
+        self.creatures = []
+
         #create new scene 
+        simWindow = mainWindow.simulation_window
         self.graphicsScene = QGraphicsScene()
-        mainWindow.simulation_window.setScene(self.graphicsScene)
+        self.graphicsScene.setSceneRect(simWindow.x(), simWindow.y(), simWindow.width() - 10, simWindow.height() - 10)
+        simWindow.setScene(self.graphicsScene)
 
         #connect buttons to functions 
         self.beginSimulationButton = mainWindow.begin_simulation_button
@@ -22,28 +34,34 @@ class SimulationView():
     
     #call the correct function based on the simulation state
     def simulate(self):
-
-        text = "Begin Simulation" if self.isSimulating else "Pause Simulation"
-        self.beginSimulationButton.setText(text)
-
         if self.isSimulating:
             self.resume()
         else:
             self.start()
-        
-        self.isSimulating = not self.isSimulating
+            self.isSimulating = True 
 
+    #draw the food items to the screen and create new food objects 
     def drawFood(self, foodAmount):
-        pass  
+        for _ in range(foodAmount):
+            food_x = randint(self.BUFFER, self.graphicsScene.width() - self.BUFFER)
+            food_y = randint(self.BUFFER, self.graphicsScene.height() - self.BUFFER)
+            newFood = Food(food_x,food_y)
+            self.food.append(newFood)
+            self.graphicsScene.addItem(newFood.pixmap)
+            newFood.pixmap.setPos(food_x, food_y)
 
+    #start the simulation 
     def start(self):
         self.drawFood(10) 
     
+    #pause the simulation
     def pause(self):
         pass 
 
+    #resume the simulation 
     def resume(self):
         pass 
     
+    #simulate to next generation only 
     def nextGeneration(self):
         pass 
