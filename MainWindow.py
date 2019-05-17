e6eb52ad6a50fc40a4070ff55cb7043fd390c6f9
Mitchel Paulin <mitchel0022@gmail.com>
2019-05-17 19:39:10 -0400

    Got the combo boxes populating again after the refactor

--- MainWindow.py
+++ MainWindow.py
@@ -15,24 +15,25 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("assets/mainwindow.ui", self)
+        self.populateCostComboBox()
         self.show()
 
-#populate and set the initial values for the QComboBoxes
-def populateCostComboBox(window):
-    functions = ['1', 'n', 'n\u00B2', 'n\u00B3']
-    speedComboBox = window.speed_cost_function_comboBox
-    sightComboBox = window.sight_cost_function_comboBox 
-    sizeComboBox = window.size_cost_function_comboBox 
+    #populate and set the initial values for the QComboBoxes
+    def populateCostComboBox(self):
+        functions = ['1', 'n', 'n\u00B2', 'n\u00B3']
+        speedComboBox = self.speed_cost_function_comboBox
+        sightComboBox = self.sight_cost_function_comboBox 
+        sizeComboBox = self.size_cost_function_comboBox 
 
-    for func in functions: 
-        speedComboBox.addItem(func)
-        sightComboBox.addItem(func)
-        sizeComboBox.addItem(func)
+        for func in functions: 
+            speedComboBox.addItem(func)
+            sightComboBox.addItem(func)
+            sizeComboBox.addItem(func)
 
-    #recommended default starting values 
-    speedComboBox.setCurrentIndex(2)
-    sightComboBox.setCurrentIndex(1)
-    sizeComboBox.setCurrentIndex(3)
+        #recommended default starting values 
+        speedComboBox.setCurrentIndex(2)
+        sightComboBox.setCurrentIndex(1)
+        sizeComboBox.setCurrentIndex(3)
 
 
 if __name__ == "__main__":
